90b4ca0adb8ac7ea197aa52e0360f9b8fb76bcfb
zhugezuo <zgz627988244@gmail.com>
2016-07-08 20:27:20 +0800

    ...

--- 001.py
+++ 001.py
@@ -4,4 +4,4 @@ for i in range(1, 5):
     for j in range(1, 5):
         for k in range(1, 5):
             if (i != j) and (i != k) and (j != k):
-                print i, j, k
+                print(i, j, k)
